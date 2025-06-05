ce466502c95b6f2b0629b139056e5e31675f74d6
ambersr <amber.schalker@hva.nl>
2025-06-05 20:16:31 +0200

    Alle data opgehaald in server cases overzicht #6 #3

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ const casesEndpoint = `${directusApiBaseUrl}/wp/v2/cases`;
 const mediaEndpoint = `${directusApiBaseUrl}/wp/v2/media/4374`;
 const usersEndpoint = `${directusApiBaseUrl}/wp/v2/users`;
 
+const embedFilter = `?_embed&acf_format=standard`;
+
+const perPage = 8;
+
 // --------------------------- GET routes --------------------------------
 
 // Home
@@ -30,8 +34,26 @@ app.get("/", async function (req, res) {
 
 // Cases
 app.get("/cases", async (req, res) => {
+  const page = 1;
+
+  const casesResponse = await fetch(`${casesEndpoint}?per_page=${perPage}&page=${page}&_fields=title,slug,yoast_head_json.og_description,yoast_head_json.og_image`);
+  const casesResponseJSON = await casesResponse.json();
+
+  const mediaResponse = await fetch(`${mediaEndpoint}`);
+  const mediaResponseJSON = await mediaResponse.json();
 
   res.render("cases.liquid");
+  const totalPages = casesResponse.headers.get("X-WP-TotalPages");
+
+  console.log(`${totalPages}`);
+
+  res.render("cases.liquid", {
+    cases: casesResponseJSON,
+    media: mediaResponseJSON,
+    currentPage: page,
+    totalPages,
+  });
+});
 });
 
 // Cases detail
