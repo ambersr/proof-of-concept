eb837b985f0b874775a48ff8f0031f95fe50db44
ambersr <amber.schalker@hva.nl>
2025-06-05 22:41:41 +0200

    Code weer terug gezet render test

--- server.js
+++ server.js
@@ -1,39 +1,140 @@
-// Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
-// Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
-import express from 'express'
+// Import
+import express from "express";
+import { Liquid } from "liquidjs";
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
-import { Liquid } from 'liquidjs';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// Maak een nieuwe Express applicatie aan, waarin we de server configureren
-const app = express()
+// Express
+const app = express();
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Maak werken met data uit formulieren iets prettiger
-app.use(express.urlencoded({extended: true}))
+// Liquid
+const engine = new Liquid();
+app.engine("liquid", engine.express());
 
-// Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
-// Bestanden in deze map kunnen dus door de browser gebruikt worden
-app.use(express.static('public'))
+// Views
+app.set("views", "./views");
 
-// Stel Liquid in als 'view engine'
-const engine = new Liquid()
-app.engine('liquid', engine.express())
+// --------------------------- Algemene API's & variabelen --------------------------------
 
-// Stel de map met Liquid templates in
-// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
-app.set('views', './views')
+const directusApiBaseUrl = "https://labelvier.nl/wp-json";
+const casesEndpoint = `${directusApiBaseUrl}/wp/v2/cases`;
+const mediaEndpoint = `${directusApiBaseUrl}/wp/v2/media/4374`;
+const usersEndpoint = `${directusApiBaseUrl}/wp/v2/users`;
 
-app.get('/', function (request, response) {
-    response.render('index.liquid')
-})
+const embedFilter = `_embed=true&acf_format=standard`;
 
-app.get('/cases', async function (request, response) {
+const perPage = 8;
 
-    const apiResponse = await fetch('https://labelvier.nl/wp-json/wp/v2/cases')
-    const apiResponseJSON = await apiResponse.json() 
-  
-    response.render('cases.liquid', {cases: apiResponseJSON});
-})
+// --------------------------- GET routes --------------------------------
+
+// Home
+app.get("/", async function (req, res) {
+
+  res.render("index.liquid");
+});
+
+// Functie fetch omzetten naar JSON
+async function fetchJson(url) {
+  const response = await fetch(url);
+  const responseJSON = await response.json();
+  return responseJSON
+}
+
+// Cases
+app.get("/cases", async (req, res) => {
+  const page = 1;
+
+  const casesResponse = await fetch(`${casesEndpoint}?per_page=${perPage}&page=${page}&_fields=title,slug,yoast_head_json.og_description,yoast_head_json.og_image`);
+  const casesResponseJSON = await casesResponse.json();
+
+  const mediaResponse = await fetch(`${mediaEndpoint}`);
+  const mediaResponseJSON = await mediaResponse.json();
+
+  const totalPages = casesResponse.headers.get("X-WP-TotalPages");
+
+  console.log(`${totalPages}`);
+
+  res.render("cases.liquid", {
+    cases: casesResponseJSON,
+    media: mediaResponseJSON,
+    currentPage: page,
+    totalPages,
+  });
+});
+
+// Paginatie
+app.get("/cases/page/:pageNumber", async (req, res) => {
+  const page = req.params.pageNumber;
+
+  const casesResponse = await fetch(`${casesEndpoint}?per_page=${perPage}&page=${page}&_fields=title,slug,yoast_head_json.og_description,yoast_head_json.og_image`);
+  const casesResponseJSON = await casesResponse.json();
+
+  const totalPages = casesResponse.headers.get("X-WP-TotalPages");
+
+  res.render("cases.liquid", {
+    cases: casesResponseJSON,
+    currentPage: page,
+    totalPages,
+  });
+});
+
+// Cases detail
+app.get("/cases/:slug", async function (req, res) {
+    
+  const slug = req.params.slug;
+const submitted = req.query.submitted === 'true';
+
+  const casesdetailResponse = await fetch(`${casesEndpoint}?slug=${slug}&${embedFilter}`);
+  const casesdetailResponseJSON = await casesdetailResponse.json();
+
+  const usersResponse = await fetch(`${usersEndpoint}`);
+  const usersResponseJSON = await usersResponse.json();
+
+  res.render("cases-detail.liquid", {
+      cases: casesdetailResponseJSON,
+      users: usersResponseJSON,
+      submitted
+    });
+});
+
+// --------------------------- POST routes -----------------------------------
+
+app.post('/cases/:slug', (req, res) => {
+  const { voornaam, achternaam, email, bericht } = req.body;
+
+  const newMessage = {
+    voornaam,
+    achternaam,
+    email,
+    bericht
+  };
+
+  const filePath = path.join(__dirname, 'messages.json');
+
+  let messages = [];
+
+  if (fs.existsSync(filePath)) {
+    const fileData = fs.readFileSync(filePath, 'utf-8');
+    try {
+      messages = JSON.parse(fileData);
+    } catch (err) {
+      console.error('Fout bij JSON parse:', err);
+    }
+  }
+
+  messages.push(newMessage);
+  fs.writeFileSync(filePath, JSON.stringify(messages, null, 2), 'utf-8');
+
+  res.redirect(303, `/cases/${req.params.slug}?submitted=true`);
+});
 
 // --------------------------- Poort --------------------------------
 
