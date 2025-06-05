8a9e35c8f224ea9cb247e4cc0405a7108a5d35e1
ambersr <amber.schalker@hva.nl>
2025-06-05 21:31:36 +0200

    test server 3

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import fetch from "node-fetch";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
