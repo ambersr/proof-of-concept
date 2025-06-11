65284ec0bb0e2d13146dea5f6fcb38760801a62e
ambersr <amber.schalker@hva.nl>
2025-06-11 10:35:14 +0200

    test server

--- server.js
+++ server.js
@@ -1,139 +1,49 @@
-// Import
-import express from "express";
-import { Liquid } from "liquidjs";
+// Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
+// Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
+import express from 'express'
 
-// Express
-const app = express();
-app.use(express.static("public"));
+// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
+import { Liquid } from 'liquidjs';
 
-// Liquid
-const engine = new Liquid();
-app.engine("liquid", engine.express());
+// Maak een nieuwe Express applicatie aan, waarin we de server configureren
+const app = express()
 
-// Views
-app.set("views", "./views");
+// Maak werken met data uit formulieren iets prettiger
+app.use(express.urlencoded({extended: true}))
 
-// --------------------------- Algemene API's, variabelen en functie --------------------------------
+// Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
+// Bestanden in deze map kunnen dus door de browser gebruikt worden
+app.use(express.static('public'))
 
-const directusApiBaseUrl = "https://labelvier.nl/wp-json";
-const casesEndpoint = `${directusApiBaseUrl}/wp/v2/cases/`;
-const mediaEndpoint = `${directusApiBaseUrl}/wp/v2/media/`;
-const usersEndpoint = `${directusApiBaseUrl}/wp/v2/users/`;
-const embedFilter = `_embed=true&acf_format=standard`;
+// Stel Liquid in als 'view engine'
+const engine = new Liquid()
+app.engine('liquid', engine.express())
 
-// Functie fetch omzetten naar JSON
-async function fetchJson(url) {
-  const response = await fetch(url);
-  const responseJSON = await response.json();
-  return responseJSON
-}
+// Stel de map met Liquid templates in
+// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
+app.set('views', './views')
 
-// --------------------------- GET routes --------------------------------
-
-// Homepagina
-app.get("/", async function (req, res) {
-
-  res.render("index.liquid");
+app.get('/', function (request, response) {
+    response.render('index.liquid')
 });
 
-// Cases overzicht (met paginatie)
-app.get("/cases/page/:pageNumber", async (req, res) => {
-  const page = req.params.pageNumber;
-  const perPage = 8;
+app.get('/cases', async function (request, response) {
 
-  // Cases ophalen
-  const casesURL = `${casesEndpoint}?per_page=${perPage}&page=${page}&_fields=title,slug,yoast_head_json.og_description,yoast_head_json.og_image,acf.logo_white`;
-  console.log(casesURL)
-  const casesResponse = await fetch(casesURL);
-  const type = casesResponse.headers.get('content-type');
-  if (type == 'application/json') {
-    const casesResponseJSON = await casesResponse.json();
-    // Totaal aantal pagina’s uit headers
-    const totalPages = casesResponse.headers.get("X-WP-TotalPages");
-
-    // array met alle cases gekoppeld met media data
-    const casesWithMedia = [];
-
-    // als er een logo_white veld is
-    for (const singleCase of casesResponseJSON) {
-      let logo_white_url = null;
-
-      // als er een logo_white veld is
-      if (singleCase.acf?.logo_white) {
-        const mediaResponseJSON = await fetchJson(`${mediaEndpoint}${singleCase.acf.logo_white}`);
-        logo_white_url = mediaResponseJSON?.source_url || null;
-      }
-
-      // voeg aan de singleCase de property logo_white_url toe
-      casesWithMedia.push({ ...singleCase, logo_white_url });
-    }
-
-    // Pagina renderen
-    res.render("cases.liquid", {
-      cases: casesWithMedia,
-      currentPage: page,
-      totalPages,
-    });
-  } else {
-    const casesResponseText = await casesResponse.text();
-    console.log(casesResponseText);
-    res.render("cases.liquid", {
-      
-      });
-  }
-
+    const apiResponse = await fetch('https://labelvier.nl/wp-json/wp/v2/cases?per_page=99');
+    const apiResponseJSON = await apiResponse.json();
   
+    response.render('cases.liquid', {cases: apiResponseJSON});
 });
 
-app.get("/cases/:slug", async (req, res) => {
-  const slug = req.params.slug;
+app.get('/cases/case/:id', async function (request, response) {
 
-  // cases en users data ophalen
-  const casesdetailResponseJSON = await fetchJson(`${casesEndpoint}?slug=${slug}&${embedFilter}`);
-  const usersResponseJSON = await fetchJson(`${usersEndpoint}`);
-
-  // haal de projectleider en teamleden op uit de acf velden
-  const projectLeider = casesdetailResponseJSON[0].acf?.case_projectleider;
-  const teamLeden = casesdetailResponseJSON[0].acf?.case_team;
-
-  // koppel role en profiel afbeelding URL aan projectleider
-  if (projectLeider) {
-    const user = usersResponseJSON.find(currentUser => currentUser.id === projectLeider.ID);
-
-    // koppel de rol van de gebruiker aan de projectleider
-    projectLeider.role = user?.acf?.user_data?.role;
-
-    // als er een profielfoto is, haal dan de afbeelding op en koppel de URL (projectleider)
-    if (user?.acf?.user_data?.profile_image) {
-      const media = await fetchJson(`${mediaEndpoint}${user.acf.user_data.profile_image}`);
-      projectLeider.profile_image_url = media.source_url;
-    }
-    else {
-      projectLeider.profile_image_url = '';
-    }
-  }
-
-  // koppel role en profiel afbeelding URL aan teamleden
-  for (const teamLid of teamLeden) {
-    const user = usersResponseJSON.find(currentUser => currentUser.id === teamLid.ID);
-    teamLid.role = user?.acf?.user_data?.role;
-
-    // als er een profielfoto is, haal dan de afbeelding op en koppel de URL (teamleden)
-    if (user?.acf?.user_data?.profile_image) {
-      const media = await fetchJson(`${mediaEndpoint}${user.acf.user_data.profile_image}`);
-      teamLid.profile_image_url = media.source_url;
-    } 
-    else {
-      teamLid.profile_image_url = '';
-    }
+    const caseResponse = await fetch('https://labelvier.nl/wp-json/wp/v2/cases/' + request.params.id);
+    const caseData = await caseResponse.json();
     
-  }
+    const usersResponse = await fetch('https://labelvier.nl/wp-json/wp/v2/users/');
+    const usersData = await usersResponse.json();
 
-  res.render("cases-detail.liquid", {
-    cases: casesdetailResponseJSON,
-    projectLeider,
-    teamLeden,
-  });
+    response.render('case.liquid', {case: caseData, users: usersData });
 });
 
 // --------------------------- Poort --------------------------------
