50b5324cce35a659fdeeb9d3348c0712f34f4197
ambersr <amber.schalker@hva.nl>
2025-06-11 15:38:03 +0200

    Server opgeschoond

--- server.js
+++ server.js
@@ -27,7 +27,6 @@ async function fetchJson(url) {
     headers: {
       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Safari/605.1.15',
       'Accept': 'application/json',
-      'Accept-Language': 'en-US,en;q=0.9',
     }
   });
   const responseJSON = await response.json();
