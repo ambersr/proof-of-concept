9334da6167f213ee54701b49e07df1ce50e3b20c
ambersr <amber.schalker@hva.nl>
2025-06-15 16:18:31 +0200

    Loading state client side js contact form #13

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -1,36 +1,36 @@
-if ('fetch' in window && 'DOMParser' in window) {
+// if ('fetch' in window && 'DOMParser' in window) {
 
-document.addEventListener('submit', async function (event) {
-    const form = event.target
+// document.addEventListener('submit', async function (event) {
+//     const form = event.target
 
-    if (!form.hasAttribute('data-enhanced')) {
-        return
-    }
+//     if (!form.hasAttribute('data-enhanced')) {
+//         return
+//     }
 
-    event.preventDefault()
+//     event.preventDefault()
 
-    const contactForm = form.querySelector('.loading');
-        if (loadingDiv) {
-            contactForm.classList.add('show');
-        }
+//     const contactForm = form.querySelector('.loading');
+//         if (contactForm) {
+//             contactForm.classList.add('show');
+//         }
         
-    const contentForm = form.querySelector('.form-content');
-        if (contentForm) {
-            contentForm.classList.add('hidden');
-        }
+//     const contentForm = form.querySelector('.form-content');
+//         if (contentForm) {
+//             contentForm.classList.add('hidden');
+//         }
 
-    const response = await fetch(form.action, {
-        method: form.method,
-        body: new URLSearchParams(new FormData(form)),
-        redirect: 'follow'
-    })
+//     const response = await fetch(form.action, {
+//         method: form.method,
+//         body: new URLSearchParams(new FormData(form)),
+//         redirect: 'follow'
+//     })
 
-    const responseText = await response.text()
+//     const responseText = await response.text()
 
-    const parser = new DOMParser()
-    const responseDOM = parser.parseFromString(responseText, 'text/html')
-    const newState = responseDOM.querySelector('[data-enhanced="' + form.getAttribute('data-enhanced') + '"]')
+//     const parser = new DOMParser()
+//     const responseDOM = parser.parseFromString(responseText, 'text/html')
+//     const newState = responseDOM.querySelector('[data-enhanced="' + form.getAttribute('data-enhanced') + '"]')
 
-    form.outerHTML = newState.outerHTML
-})
-}
+//     form.outerHTML = newState.outerHTML
+// })
+// }
