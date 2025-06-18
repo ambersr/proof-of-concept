9a92ac100baee6e7639784a2a413d49d8a9a9b5b
ambersr <amber.schalker@hva.nl>
2025-06-18 19:11:50 +0200

    Hide progressbar on scroll mobile #26

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -3,15 +3,18 @@ let oudeScrollPositie = window.scrollY;
 document.addEventListener('scroll', () => {
   const huidigeScrollPositie = window.scrollY;
   const logoHeader = document.querySelector('nav.top');
+  const progressBar = document.querySelector('.progress');
 
   if (oudeScrollPositie > huidigeScrollPositie) {
 
     // wanneer de gebruiker omhoog scrollt laat menu zien
     logoHeader.classList.remove('hide');
+    progressBar.classList.remove('hide');
   } else {
 
     // wanneer de gebruiker omlaag scrollt hide menu
     logoHeader.classList.add('hide');
+    progressBar.classList.add('hide');
   }
 
   // Update de scrollpositie
